b944410eb9fc005312e53514bd6e312c5b7fbdf0
Moncef <moncefelhilali@gmail.com>
2024-10-17 00:04:31 +0100

    Add Update method

--- HAHN.Application/Tickets/Commands/UpdateTicket.cs
+++ HAHN.Application/Tickets/Commands/UpdateTicket.cs
@@ -4,26 +4,27 @@ using MediatR;
 
 namespace HAHN.Application.Tickets.Commands
 {
-    public class CreateTicket
+    public class UpdateTicket
     {
         public class Command : IRequest<Ticket?>
         {
+            public int Id { get; set; }
             public Ticket Ticket { get; set; }
         }
 
         public class Handler : IRequestHandler<Command, Ticket?>
         {
             private readonly ITicketRepository _repository;
-            public Handler(ITicketRepository repository)
+            public Handler(ITicketRepository repository) 
             {
                 _repository = repository;
             }
 
             public async Task<Ticket?> Handle(Command request, CancellationToken cancellationToken)
             {
-                var ticket = await _repository.CreateAsync(request.Ticket);
+                var ticket = await _repository.UpdateAsync(request.Id, request.Ticket);
                 // add validation
-                return null;
+                return ticket;
             }
         }
     }
